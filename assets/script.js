77db486e16dd539a82e74ef704820e1e37debe66
Lauren Plenger <laurenb08@gmail.com>
2020-11-11 20:18:42 -0800

    fixed API issue

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,25 @@
-var city = $("#cities").val();
-var singleDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d30adb1683f1aae50c1159cab23299c7";
-var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=d30adb1683f1aae50c1159cab23299c7";
+// var city = $("#citiesSearch").val().trim();
+// var apiKey = "d30adb1683f1aae50c1159cab23299c7";
+// var singleDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+// var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
-$("#button-addon2").on("click", search)
+$("#search").on("click", search)
 
 function search() {
     // request(singleDayURL);
-
+    var city = $("#citiesSearch").val();
+    var apiKey = "d30adb1683f1aae50c1159cab23299c7";
+    var singleDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    // var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+    console.log(city);
     $.ajax({
         url: singleDayURL,
         method: "GET"
     }).then(function (response) {
-        $("#cities").text(JSON.stringify(response));
+        // $("#citiesSearch").text(JSON.stringify(response));
+        console.log(response);
     });
 };
 
-console.log(city);
-console.log(singleDayURL);
-console.log(response);
+// console.log(city);
+// console.log(singleDayURL);
