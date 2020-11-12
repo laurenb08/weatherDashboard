052938257d5b52ed07e0b442a954e40d9470979e
Lauren Plenger <laurenb08@gmail.com>
2020-11-11 22:07:44 -0800

    added function to get the 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,12 @@
-// var city = $("#citiesSearch").val().trim();
-// var apiKey = "d30adb1683f1aae50c1159cab23299c7";
-// var singleDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-// var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
-$("#search").on("click", search)
+$("#search").on("click", daySearch)
 
-function search() {
+function daySearch() {
     // request(singleDayURL);
     var city = $("#citiesSearch").val();
     var apiKey = "d30adb1683f1aae50c1159cab23299c7";
     var singleDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-    // var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+    var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
     console.log(city);
     $.ajax({
         url: singleDayURL,
@@ -19,7 +15,15 @@ function search() {
         // $("#citiesSearch").text(JSON.stringify(response));
         console.log(response);
     });
-};
 
-// console.log(city);
-// console.log(singleDayURL);
+    forecastSearch();
+
+    function forecastSearch() {
+        $.ajax({
+            url: fiveDayURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        });
+    }
+};
