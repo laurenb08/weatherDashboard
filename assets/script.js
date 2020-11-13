3bdcf32a2c77c0076a217b6f0a821f1c68e7c6bf
Lauren Plenger <laurenb08@gmail.com>
2020-11-13 00:58:56 -0800

    added button property to li items in html

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ $("#search").on("click", daySearch)
 function daySearch() {
     // request(singleDayURL);
     var city = $("#citiesSearch").val();
+
     var apiKey = "d30adb1683f1aae50c1159cab23299c7";
     var singleDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
     var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
