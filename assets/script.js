4c06a1ff60a3424c62847e664f10002270ab3643
Lauren Plenger <laurenb08@gmail.com>
2020-11-12 21:28:18 -0800

    added data from API to HTML

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,9 @@ function daySearch() {
     var apiKey = "d30adb1683f1aae50c1159cab23299c7";
     var singleDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
     var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+
+    // var uvRUL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + searchLat + "&lon=" + searchLong + "&appid=" + apiKey;
+
     console.log(city);
     $.ajax({
         url: singleDayURL,
@@ -14,16 +17,48 @@ function daySearch() {
     }).then(function (response) {
         // $("#citiesSearch").text(JSON.stringify(response));
         console.log(response);
+
+        //convert temp to farenheit
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+        //transfer content to HTML
+        $(".city").html("<h1>" + response.name + " Weather</h1>");
+        // var tempF = (response.main.temp - 273.15) *1.80 + 32;
+        $(".temp").text("Temperature: " + tempF.toFixed(2));
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+
+        // get UV Index
+        var searchLat = response.coord.lat;
+        var searchLong = response.coord.lon;
+        console.log(searchLat);
+        console.log(searchLong);
+        // var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + searchLat + "&lon=" + searchLong + "&appid=" + apiKey;
+
+        // function uvIndex() {
+        //     $.ajax({
+        //         url: singleDayURL,
+        //         method: "GET"
+        //     }).then(function (response) {
+        //         console.log(response);
+        //         $(".uvIndex").text("UV Index: " + uvRUL);
+        //     })
+        // };
+
+        forecastSearch();
+
+        function forecastSearch() {
+            $.ajax({
+                url: fiveDayURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+
+                //convert temp to farenheit
+
+
+                //transfer content to HTML
+            });
+        }
     });
-
-    forecastSearch();
-
-    function forecastSearch() {
-        $.ajax({
-            url: fiveDayURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-        });
-    }
-};
+}
